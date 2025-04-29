90b4b52b64ca5f916a62db2147b227a82833003a
Sergei Filipchenko <s.e.r.e.g.a.-93@mail.ru>
2025-04-29 19:45:43 +0500

    Added methods to main_page

--- pages/main_page.py
+++ pages/main_page.py
@@ -17,26 +17,50 @@ class MainPageYandexScooter:
     def click_drop_down_list_important_questions_1(self):
         self.driver.find_element(*MainPageLocators.DROP_DOWN_LIST_IMPORTANT_QUESTIONS_1).click()
 
+    def receive_text_answer_question_1(self):
+        self.driver.find_element(*MainPageLocators.TEXT_ANSWER_QUESTION_1).text()
+
     def click_drop_down_list_important_questions_2(self):
         self.driver.find_element(*MainPageLocators.DROP_DOWN_LIST_IMPORTANT_QUESTIONS_2).click()
+
+    def receive_text_answer_question_2(self):
+        self.driver.find_element(*MainPageLocators.TEXT_ANSWER_QUESTION_2).text()
 
     def click_drop_down_list_important_questions_3(self):
         self.driver.find_element(*MainPageLocators.DROP_DOWN_LIST_IMPORTANT_QUESTIONS_3).click()
 
+    def receive_text_answer_question_3(self):
+        self.driver.find_element(*MainPageLocators.TEXT_ANSWER_QUESTION_3).text()
+
     def click_drop_down_list_important_questions_4(self):
         self.driver.find_element(*MainPageLocators.DROP_DOWN_LIST_IMPORTANT_QUESTIONS_4).click()
+
+    def receive_text_answer_question_4(self):
+        self.driver.find_element(*MainPageLocators.TEXT_ANSWER_QUESTION_4).text()
 
     def click_drop_down_list_important_questions_5(self):
         self.driver.find_element(*MainPageLocators.DROP_DOWN_LIST_IMPORTANT_QUESTIONS_5).click()
 
+    def receive_text_answer_question_5(self):
+        self.driver.find_element(*MainPageLocators.TEXT_ANSWER_QUESTION_5).text()
+
     def click_drop_down_list_important_questions_6(self):
         self.driver.find_element(*MainPageLocators.DROP_DOWN_LIST_IMPORTANT_QUESTIONS_6).click()
+
+    def receive_text_answer_question_6(self):
+        self.driver.find_element(*MainPageLocators.TEXT_ANSWER_QUESTION_6).text()
 
     def click_drop_down_list_important_questions_7(self):
         self.driver.find_element(*MainPageLocators.DROP_DOWN_LIST_IMPORTANT_QUESTIONS_7).click()
 
+    def receive_text_answer_question_7(self):
+        self.driver.find_element(*MainPageLocators.TEXT_ANSWER_QUESTION_7).text()
+
     def click_drop_down_list_important_questions_8(self):
         self.driver.find_element(*MainPageLocators.DROP_DOWN_LIST_IMPORTANT_QUESTIONS_8).click()
+
+    def receive_text_answer_question_8(self):
+        self.driver.find_element(*MainPageLocators.TEXT_ANSWER_QUESTION_8).text()
 
     def wait_element_top_order_button(self):
         WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located(*MainPageLocators.TOP_ORDER_BUTTON))
